d1e86c0b649713545d7088826157a05a9ca62ed7
spoorthipothanaka-a11y <spoorthipothanaka@gmail.com>
2026-07-11 15:37:18 +0530

    Add files via upload

---
+++ codespace/Scope.java
@@ -0,0 +1,15 @@
+class Scope{
+  public static void main(String args[]){
+  String name="Spoorthi";
+  int age=19;
+  System.out.println(name);
+  System.out.println(age+"years old");
+  if(age>=18){
+     String status="Allowed for vote";
+     System.out.println(status);
+  }else{
+     String status="Not allowed for vote";
+     System.out.println(status);
+     }
+  }
+}
