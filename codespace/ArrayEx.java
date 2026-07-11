7285c0e948a49b9069fe9665cacd71d80eeb0ca0
spoorthipothanaka-a11y <spoorthipothanaka@gmail.com>
2026-07-11 15:17:24 +0530

    Add files via upload

---
+++ codespace/ArrayEx.java
@@ -0,0 +1,10 @@
+class ArrayEx {
+  public static void main(String args[]){
+  String[]name=new String[5];
+  name[0]="Sainandini";
+  name[1]="Praveena";
+  name[2]="Kushi";
+  name[3]="Spoorthi";
+  System.out.println(name[2]);
+  }
+}
