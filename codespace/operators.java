c4cf5ddc098af2ced2b3f251cfda33e3f7ae62b7
spoorthipothanaka-a11y <spoorthipothanaka@gmail.com>
2026-07-11 15:59:39 +0530

    Add files via upload

---
+++ codespace/operators.java
@@ -0,0 +1,11 @@
+class operators {
+  public static void main(String args[]){
+  int a=12;
+  int b=10;
+  System.out.println("Bitwise AND: "+(a&b));
+  System.out.println("Bitwise OR: "+(a|b));
+  System.out.println("Bitwise XOR: "+(a^b));
+  int c=5;
+  System.out.println("Bitwise NOT: "+(~c));
+  }
+}
