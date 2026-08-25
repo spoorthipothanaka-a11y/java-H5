688ec71ba568dbfc26de1c17d385262c91d357aa
spoorthipothanaka-a11y <spoorthipothanaka@gmail.com>
2026-08-25 15:01:23 +0530

    Add files via upload

---
+++ codespace/PrimitiveDataTypes.java
@@ -0,0 +1,20 @@
+public class PrimitiveDataTypes{
+	public static void main(String[] args){
+		byte byteValue=100;
+		short shortValue=1000;
+		int intValue=10000;
+		long longValue=100000L;
+		float floatValue=30.4f;
+		double doubleValue=20.3562;
+		char charValue='A';
+		boolean booleanValue=true;
+		System.out.println("byte:"+byteValue);
+		System.out.println("short:"+shortValue);
+		System.out.println("int:"+intValue);
+		System.out.println("long:"+longValue);
+		System.out.println("float:"+floatValue);
+		System.out.println("double:"+doubleValue);
+		System.out.println("char:"+charValue);
+		System.out.println("boolean:"+booleanValue);
+	}
+}
