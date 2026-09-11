597edcfa46c3d1f8e71c10d542470bda63e3ed95
spoorthipothanaka-a11y <spoorthipothanaka@gmail.com>
2026-09-11 15:39:00 +0530

    Add files via upload

---
+++ codespace/ConditionalStatements.java
@@ -0,0 +1,30 @@
+public class ConditionalStatements{
+	public static void main(String[] args){
+	int age=20;
+	if(age>=18){
+		System.out.println("Eligible to vote");
+	}
+	int num=10;
+	if(num%2==0){
+		System.out.println("Number is Even");
+	}else{
+		System.out.println("Number is Odd");
+	}
+	int marks=95;
+	if(marks>=90){
+		System.out.println("Grade A+");
+	}else if(marks>=80){
+			System.out.println("Grade A");
+	}else if(marks>=70){
+			System.out.println("Grade B");
+	}else if(marks>=80){
+			System.out.println("Grade C");
+	}else{
+			System.out.println("Fail");
+	}
+	}
+}
+		
+		
+		
+		
